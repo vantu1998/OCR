ae0e6af6de8f2f57b835519a277cab8a551e33db
Tu Nguyen <vantu1998@gmail.com>
2020-08-16 22:43:52 +0700

    build thanh cong

--- src/main/java/com/example/demo/OcrApplication.java
+++ src/main/java/com/example/demo/OcrApplication.java
@@ -2,14 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @SpringBootApplication
-public class OcrApplication {
+public class OcrApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcrApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OcrApplication.class);
 	}
 
 }
